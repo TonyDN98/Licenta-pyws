042ec7335ca4a372da3cab7199c18307ea9c3c61
kheop <kh3ops97@gmail.com>
2022-08-19 09:44:13 +0300

    UserModel

---
+++ models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const UserSchema = new Schema({
+    email: {
+        type:String,
+        required : true,
+        unique:true
+    }
+});
+
+UserSchema.plugin(passportLocalMongoose); // give some additional () to our UserSchema, also complete with fields password our schema;
+
+module.exports = mongoose.model('User', UserSchema);
