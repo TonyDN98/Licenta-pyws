bb17985dc45989e20600f66c073b84be28207fdf
kheop <kh3ops97@gmail.com>
2022-08-21 17:39:52 +0300

    Images

--- public/javascripts/validateForms.js
+++ public/javascripts/validateForms.js
@@ -3,6 +3,9 @@
 (function () {
     'use strict'
 
+    bsCustomFileInput.init(); //bsCustomFile Form Validator
+
+
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
     const forms = document.querySelectorAll('.validated-form')
 
