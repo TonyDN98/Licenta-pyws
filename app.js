ffa939cc92cde59b11d0243b92db69908f3f73e1
kheop <kh3ops97@gmail.com>
2022-08-24 10:40:13 +0300

    Changed port env try to deploy Heroku and start script from JSON

--- app.js
+++ app.js
@@ -134,16 +134,13 @@ app.use((err, req, res, next) => {
 })
 
 // TODO: Server Port
-// app.listen(3000, () => {
-//     console.log('Serving on port 3000')
-// })
 
+const port = process.env.PORT || 3000;
+// app.listen(3000, "127.0.0.1", function(){
+//     console.log("The  PlacesYouShare Server Has Started!");
+// });
 
-app.listen(3000, "127.0.0.1", function(){
-    console.log("The  PlacesYouShare Server Has Started!");
-});
-// const port = process.env.PORT || 3000;
-// app.listen(port, () => {
-//     console.log(`Serving on port ${port}`)
-// })
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
+})
 
